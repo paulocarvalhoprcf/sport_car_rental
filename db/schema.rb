1ef2eb1d8be9bb2f32560e66a87f4d755ec52587
Luis Osorio <losorio1986@gmail.com>
2021-10-27 18:34:47 -0300

    adicionado a coluna user_id ao car, ajustes no pundit, criação do form da view do /cars new, ajustes no view do show

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_27_150114) do
+ActiveRecord::Schema.define(version: 2021_10_27_204010) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,6 +35,8 @@ ActiveRecord::Schema.define(version: 2021_10_27_150114) do
     t.integer "price"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_cars_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -52,4 +54,5 @@ ActiveRecord::Schema.define(version: 2021_10_27_150114) do
 
   add_foreign_key "bookings", "cars"
   add_foreign_key "bookings", "users"
+  add_foreign_key "cars", "users"
 end
