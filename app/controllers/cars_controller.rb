1a9ac508ed4495f8a594f2198a70b188e3597bb3
Luis Osorio <losorio1986@gmail.com>
2021-10-27 16:41:52 -0300

    implementado o login do usuário e as funcionalidades do pundit

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -1,23 +1,41 @@
 class CarsController < ApplicationController
+  before_action :set_restaurant, only: [:show, :destroy]
+
   def index
-    @cars = Car.all
+    @cars = policy_scope(Car)
   end
 
   def show
-    @car = Car.find(params[:id])
+    authorize @car
   end
 
   def new
     @car = Car.new
+    authorize @car
   end
 
   def create
     @car = Car.new(car_params)
-    @car.save!
+    @car.user = current_user
+    authorize @car
+
+    if @car.save
+      redirect_to @car, notice: 'Car was successfully created.'
+    else
+      render :new
+    end
     redirect_to car_path(@car)
   end
 
+  def destroy
+    @car.destroy
+    redirect_to cars_url, notice: 'Car was successfully destroyed.'
+  end
+
   private
+  def set_car
+    @car = Car.find(params[:id])
+  end
 
   def car_params
     params.require(:car).permit(:model, :manufacturer, :year, :img_url, :price)
