d0101db761a9a69e62f25cb26c2ffebb155b3e04
paulocarvalho <carvalhofilhopr@gmail.com>
2021-10-29 17:16:20 -0300

    logic-search

--- app/models/car.rb
+++ app/models/car.rb
@@ -2,4 +2,11 @@ class Car < ApplicationRecord
   has_many :bookings, dependent: :destroy
 
   validates :model, :manufacturer, :price, presence: true
+
+  include PgSearch::Model
+  pg_search_scope :search_by_model_and_manufacturer,
+    against: [ :model, :manufacturer],
+    using: {
+      tsearch: { prefix: true }
+    }
 end
