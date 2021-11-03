e2eabfeb0322edb098534ec2eb559f0ce03a331b
Luis Osorio <losorio1986@gmail.com>
2021-11-03 14:44:06 -0300

    adicionado o avatar na navbar

--- app/javascript/components/banner.js
+++ app/javascript/components/banner.js
@@ -2,7 +2,7 @@ import Typed from 'typed.js';
 
 const loadDynamicBannerText = () => {
   new Typed('#banner-typed-text', {
-    strings: ["BMW", "Mercedes", "Porsche", "Ferrari", "Lamborghini", "Car"],
+    strings: ["", "BMW", "Mercedes", "Porsche", "Ferrari", "Lamborghini", "Car"],
     typeSpeed: 100,
     loop: false
   });
