58548e47e8a1bc46650c310c193d8ae0e026e663
paulocarvalho <carvalhofilhopr@gmail.com>
2021-11-03 14:17:45 -0300

    car-sound-and-booking-block

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -3,6 +3,7 @@ class Booking < ApplicationRecord
   belongs_to :car
 
   validates :starting_date, :ending_date, presence: true
+  validates :starting_date, :ending_date, uniqueness: { scope: :car_id, message: "Sorry, this car is not available anymore!" }
   validates_datetime :starting_date, on_or_after: :today
   validates_datetime :ending_date, after: :starting_date
 end
